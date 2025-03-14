2a86fbab525dc375a95fe7369d9d82c0a70d9d23
Oceaan Pendharkar <122117462+oceaan-pendharkar@users.noreply.github.com>
2025-03-13 18:28:26 -0700

    Change port.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(cors()); // Allows all origins
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
