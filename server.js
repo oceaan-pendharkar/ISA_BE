b36ed35b2d30d89ad56957e1517755604c983631
Justin Saint <justinsaintdev@gmail.com>
2025-03-15 12:49:45 -0700

    Added password hashing for login and register functions using bcrypt

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 import express from "express";
 import { getUserByEmail, createUser } from "./db.js";
 import cors from "cors";
+import bcrypt from "bcrypt";
 
 const app = express();
 app.use(cors()); // Allows all origins
 
 const PORT = process.env.PORT || 3001;
+const saltRounds = 12;
 
 app.use(express.json());
 
+// Login an existing user
 app.post("/isa-be/ISA_BE/login", async (req, res) => {
   console.log("Received login request:", req.body); // Debugging log
 
   const user = await getUserByEmail(req.body.email);
   if (!user) return res.status(404).json({ error: "User not found" });
 
-  //TODO Justin: check hashed password
+  // Check hashed password
+  const passwordCheck = await bcrypt.compare(req.body.password, user.password_hash);
+  if(!passwordCheck) return res.status(401).json({ error: "Invalid password"});
+
+  // Prints the bcypt hash
   console.log(user.password_hash);
 
+  console.log("Login successful for:", user.email); // Debugging log
   res.json({ id: user.id, email: user.email });
 });
 
+// Register a new user
 app.post("/isa-be/ISA_BE/register", async (req, res) => {
   const { email, password } = req.body;
   console.log("Received login request:", req.body); // Debugging log
@@ -28,13 +37,15 @@ app.post("/isa-be/ISA_BE/register", async (req, res) => {
   if (!email || !password)
     return res.status(400).json({ error: "Missing fields" });
 
-  //TODO Justin: hash password
-  let password_hash = password + "HASH"; //placeholder - Justin delete pls
-
   try {
-    const newUser = await createUser(email, password_hash); // Password should be hashed before calling this
+    // Hash entered password
+    const password_hash = await bcrypt.hash(password, saltRounds);
+
+    // Create new user
+    const newUser = await createUser(email, password_hash);
     console.log("new user registered: ", newUser);
     res.json(newUser);
+
   } catch (err) {
     res.status(500).json({ error: "Error creating user" });
   }
